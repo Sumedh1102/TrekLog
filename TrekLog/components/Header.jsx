a5efe0ef02f9c1ff1fb45d484105a8005c33270c
SUMEDH1234567 <124816595+Sumedh1102@users.noreply.github.com>
2025-03-24 20:21:19 +0530

    React travel journal with props

---
+++ TrekLog/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+
+       <header>
+        <img src="globe.png" className="globe" alt="globe image "/>
+        <h1>My travel journey</h1>
+       </header>
+       
+    )
+}
